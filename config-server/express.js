a2008ee39f0cb977121a23a2c5aa80ad78d388c1
Eduardo <eduardo.esteves@original.io>
2020-01-20 13:37:28 -0300

    ajusta api

--- config-server/express.js
+++ config-server/express.js
@@ -13,12 +13,138 @@ module.exports = function(){
     return res.send('pong');
   });
 
-  consign()
-    .include('api')
-    .then('routes')
-    .into(app);
+  var api = {}
 
-  app.get('*', function (req, res) {
+  api.controle = function(req, res){
+    var controle = [
+      {
+        "nome": "Camile",
+        "meses": [
+          {"janeiro": "pago"},
+          {"fevereiro": "pago"},
+          {"março": "pago"},
+          {"abril": "pago"},
+          {"maio": "pago"},
+          {"junho": ""},
+          {"julho": ""},
+          {"agosto": ""},
+          {"setembro": ""},
+          {"outubro": ""},
+          {"novembro": ""},
+          {"dezembro": ""}
+        ],
+        "status": true,
+        "avatar": 'avatar-camile.jpg'
+      },
+      {
+        "nome": "Eduardo",
+        "meses": [
+          {"janeiro": "pago"},
+          {"fevereiro": "pago"},
+          {"março": "pago"},
+          {"abril": "pago"},
+          {"maio": "pago"},
+          {"junho": "pago"},
+          {"julho": "pago"},
+          {"agosto": "pago"},
+          {"setembro": "pago"},
+          {"outubro": "pago"},
+          {"novembro": "pago"},
+          {"dezembro": "pago"}
+        ],
+        "status": true,
+        "avatar": 'avatar-eduardo.jpg'
+      },
+      {
+        "nome": "Géssica",
+        "meses": [
+          {"janeiro": ""},
+          {"fevereiro": ""},
+          {"março": ""},
+          {"abril": ""},
+          {"maio": ""},
+          {"junho": ""},
+          {"julho": ""},
+          {"agosto": ""},
+          {"setembro": ""},
+          {"outubro": ""},
+          {"novembro": ""},
+          {"dezembro": ""}
+        ],
+        "status": true,
+        "avatar": 'avatar-gessica.jpg'
+      },
+      {
+        "nome": "Gisele",
+        "meses": [
+          {"janeiro": "pago"},
+          {"fevereiro": "pago"},
+          {"março": "pago"},
+          {"abril": "pago"},
+          {"maio": "pago"},
+          {"junho": "pago"},
+          {"julho": "pago"},
+          {"agosto": "pago"},
+          {"setembro": "pago"},
+          {"outubro": "pago"},
+          {"novembro": "pago"},
+          {"dezembro": "pago"}
+        ],
+        "status": "Em dia",
+        "avatar": 'avatar-gisele.jpg'
+      },
+      {
+        "nome": "Iago",
+        "meses": [
+          {"janeiro": ""},
+          {"fevereiro": ""},
+          {"março": ""},
+          {"abril": ""},
+          {"maio": ""},
+          {"junho": ""},
+          {"julho": ""},
+          {"agosto": ""},
+          {"setembro": ""},
+          {"outubro": ""},
+          {"novembro": ""},
+          {"dezembro": ""}
+        ],
+        "status": true,
+        "avatar": 'avatar-iago.jpg'
+      },
+      {
+        "nome": "Victor",
+        "meses": [
+          {"janeiro": ""},
+          {"fevereiro": ""},
+          {"março": ""},
+          {"abril": ""},
+          {"maio": ""},
+          {"junho": ""},
+          {"julho": ""},
+          {"agosto": ""},
+          {"setembro": ""},
+          {"outubro": ""},
+          {"novembro": ""},
+          {"dezembro": ""}
+        ],
+        "status": true,
+        "avatar": 'avatar-victor.jpg'
+      },
+    ]
+
+    res.json(controle);
+
+  };
+
+  app.get('/v1/controle', api.controle);
+
+  // consign()
+  //   .include('api')
+  //   .then('routes')
+  //   .into(app);
+
+  app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, '../build', 'index.html'));
   });
 
